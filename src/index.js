961f0c1381d9735aee10caed428e822bf007809e
Mats Stenwreth <mats.stenwreth@gmail.com>
2022-07-31 23:19:55 +0200

    just made some minor changes

--- src/index.js
+++ src/index.js
@@ -3,26 +3,53 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const fs = require('fs')
+const stream = require('stream')
+const readline = require('readline')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
 app.use('/client', express.static('public'))
 
-const { getMostCommonWord, getAllWordsArray } = require('./utils/_helper')
+const { getMostCommonWord, parseLine, replaceMostCommonWord } = require('./utils/_helper')
 
-app.post('/file', (req, res) => {
+app.post('/upload', async (req, res) => {
     const { files } = req
 
     if(!files) return res.status(404)
 
-    const text = files.text.data.toString('utf-8')
-    const mostCommonWord = getMostCommonWord(text)
-    let all = getAllWordsArray(text)
+    const textFile = files.textFile.data
+    if (!textFile instanceof Buffer) {
+        console.log('Generic file error', textFile)
+        res.send(500).json({ message: 'Internal error'})
+    }
 
-    all.forEach((e, i) => { if (e === mostCommonWord) all[i] = `foo${e}bar` })
-
-    res.status(200).json({ mostCommonWord, text: files.text.size === 0 ? 'Missing content in file' : all })
+    try {
+        const bufferStream = new stream.PassThrough()
+        bufferStream.end(textFile)
+    
+        let rl = readline.createInterface({
+            input: bufferStream
+        })
+    
+        let text = ''
+        rl.on('line', (line) => {
+            line = parseLine(line)
+            if (line.length > 0) {
+                text += line.trim().toLowerCase() + ' '
+            }
+        })
+    
+        rl.on('close', () => {
+            const mostCommonWord = getMostCommonWord(text)
+            text = replaceMostCommonWord(text, mostCommonWord)
+            return res.status(200).json({ mostCommonWord, 'text': files.textFile.size === 0 ? 'Missing content in file' : text })
+        }) 
+    } catch (error) {
+        console.log(error.message)
+        res.send(500).json({ message: 'Internal error' })
+    }
 })
 
 app.listen(port, () => {
--- src/utils/_helper.js
+++ src/utils/_helper.js
@@ -1,43 +1,54 @@
 const getUniqueWords = (textBlob) => {
-    const wordsList = getAllWordsArray(textBlob)
-
-    const onlyUnique = (value, index, self) => {
-        return self.indexOf(value) === index
-    }
-
-    return wordsList.filter(onlyUnique)
+    const wordsList = getAllWordsAsArray(textBlob)
+    return [... new Set(wordsList)]
 }
-
+ 
+/**
+ * @param words string of all textcontent from a file
+ */
 const getMostCommonWord = (words) => {
-    const allWords = getAllWordsArray(words)
+    const allWordsArray = getAllWordsAsArray(words)
     const uniqueWords = getUniqueWords(words)
+    console.log('uniqueWords1', uniqueWords)
     
     const collection = new Map()
 
     for (let word of uniqueWords) {
-        let counter = allWords.filter(e => e.toLowerCase() === word.toLowerCase()).length
+        let counter = allWordsArray.filter(e => e === word).length
         if(counter > 0) collection.set(word, counter)
     }
+
     const result = new Map([...collection].sort((a, b) => b[1] - a[1]))
     const [mostCommon] = result.keys()
-     
+    
     return mostCommon
 }
 
-const getAllWordsArray = (textBlob) => {
-    const regexWordsSpaces = /[^\w\s]/gi
-    const regexStrangContext = /\w_|_/gi
+const getAllWordsAsArray = (textBlob) => {
     return textBlob
-        .replace(/\n/g, ' ')
-        .replace(regexWordsSpaces, '')
-        .replace(regexStrangContext, '')
         .split(' ')
         .filter(e => e)
         .map(e => e.toLowerCase())
 }
 
+const parseLine = (line) => {
+    return line
+        .trim()
+        .replace(/[^\x20-\x7E]/g, ' ') // remove ascii characters
+        .replace(/\W|_/g, ' ') // remove special characters
+        .replace(/  +/g, ' ') // replace multiple spaces
+}
+
+const replaceMostCommonWord = (text, word) => {
+    const words = getAllWordsAsArray(text)
+
+    return words.map(e => e === word ? e.replace(word, `foo${word}bar`) : e ).join(' ')
+}
+
 module.exports = {
     getUniqueWords,
     getMostCommonWord,
-    getAllWordsArray
+    getAllWordsAsArray,
+    parseLine,
+    replaceMostCommonWord
 }
